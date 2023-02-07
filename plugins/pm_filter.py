7bbd53f547f8bdd3328d7afef233d3a48d7f2f1a
Vikings70 <119702395+Vikings70@users.noreply.github.com>
2023-02-07 19:45:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -20,10 +20,6 @@ from database.filters_mdb import (
     find_filter,
     get_filters,
 )
-
-import os
-req_channel = int(os.environ.get('REQ_CHANNEL','-1001695328567'))
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -630,7 +626,6 @@ async def auto_filter(client, msg, spoll=False):
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
              if not files:
-                 await client.send_message(req_channel, f"#REQUESTED_LOGS \n\nCONTENT NAME:**`{search}` \n**REQUESTED BY :** {message.from_user.first_name}\n**USER ID :** {message.from_user.id}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🏹 Mark as Done 🦋", callback_data="close_data")]]))
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg)
                 else:
